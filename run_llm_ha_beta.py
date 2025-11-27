3739aaa58fe9c1e40c2ab48d727ed52448ab5494
pengqianhan <hanpengqian@outlook.com>
2025-11-27 15:52:13 +1300

    Refactor initial hybrid automaton specification handling in run_llm_ha_beta.py
    - Removed the conditional logic for initial_ha_spec and directly integrated the specification into the task prompt.
    - Introduced a new prompts_ha module to manage the initial hybrid automaton specification format.
    - Added initial_ha_spec_prompt in prompts.py to standardize the hybrid automaton template structure.

--- run_llm_ha_beta.py
+++ run_llm_ha_beta.py
@@ -246,58 +246,8 @@ Output Requirements:
         task += SELF_IS_CODE_AGENT_PROMPT
 
     # Add initial HA specification if provided
-    if initial_ha_spec:
-        task += f"""
+    task += f"""\n\n# Initial Hybrid Automaton Specification (v0)\n{initial_ha_spec}\n\n# Task\nGenerate an improved version (v1) based on the analysis."""
 
-# Initial Hybrid Automaton Specification (v0)
-PROMPT_HA_v0 = {initial_ha_spec}
-
-# Task
-Generate an improved version (v1) based on the analysis."""
-    else:
-        # Add default template
-        task += """
-
-# Hybrid Automaton Template
-PROMPT_HA_v0 = {
-    "automaton": {  # automaton
-        "var": "x1",  # variables list, separated by ','
-        "input": "u1",  # input list, separated by ','
-        "mode": [  # mode list
-            {
-                "id": 1,  # mode id
-                "eq": "x1[1] = x1[0] + u1"
-                # k-th order differential equation in the mode, separated by ','
-                # cannot contain variables that are not defined in var, x[0] represents the original value of x, and x[k] is the k-th derivative
-                # The left side of the equal sign is the highest order derivative
-                # The right side is the expression, does not support implicit functions
-                # Any variable 'x' MUST be in the format x[k]
-                # MUST provide ode for each variable
-            }
-        ],
-        "edge": [
-            {
-                "direction": "1 -> 1",  # edge from mode u to mode v, represented as 'u -> v'
-                "condition": "x1 >= 5",  # transition condition, cannot contain variables that are not defined in var
-                "reset": {  # reset mapping for each variable, each variable has a list of reset values
-                    "x1": [
-                        "",
-                        "x1[0]"
-                    ]
-                }
-            }
-        ]
-    },
-    "config": {  # configuration parameters
-        "dt": 0.001,  # discrete time step
-        "total_time": 10.0,  # total sampling time
-        "dim": 1,  # dimension of ODE in "eq" field
-        "non_linear_items": ""  # additional nonlinear or cross terms in "eq" field
-    }
-}
-
-# Task
-Generate an improved version (v1) based on the analysis."""
     system_config_prompt = f"""\n\n
 System Configuration:
 - System Name: {{{system_name}}}
