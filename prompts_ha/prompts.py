09ff9bca6c1465e269c017b42208aeb5f01d3242
pengqianhan <hanpengqian@outlook.com>
2025-11-28 11:38:20 +1300

    Update non-linear items in initial hybrid automaton specification from "x[?] ** 5" to "x[?] ** 3" for revised mathematical modeling.

--- prompts_ha/prompts.py
+++ prompts_ha/prompts.py
@@ -29,5 +29,5 @@ initial_ha_spec_prompt = """{
         "total_time": 10.0,  # total sampling time
         "dim": 1,  # dimension of ODE in "eq" field
         "need_reset": true, # whether to reset the state of the system when the transition is triggered
-        "non_linear_items": "x[?] ** 5"  # additional nonlinear or cross terms in "eq" field
+        "non_linear_items": "x[?] ** 3"  # additional nonlinear or cross terms in "eq" field
     } }"""
